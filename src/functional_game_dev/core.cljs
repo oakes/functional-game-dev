198c25c2764d404377ab4b25bd3c6ade0ef837d3
oakes <zsoakes@gmail.com>
2016-09-14 20:20:20 -0400

    Initial import

---
+++ src/functional_game_dev/core.cljs
@@ -0,0 +1,92 @@
+(ns functional-game-dev.core
+  (:require [play-cljs.core :as p]
+            [functional-game-dev.state :as s]
+            [functional-game-dev.utils :as u]))
+
+(defonce game (p/create-game u/view-size u/view-size))
+(defonce state (atom nil))
+
+(def smiley
+  [:fill {:color "yellow"}
+   [:ellipse {:width 100 :height 100}
+    [:fill {:color "black"}
+     [:ellipse {:x -20 :y -10 :width 10 :height 10}]
+     [:ellipse {:x 20 :y -10 :width 10 :height 10}]]
+    [:fill {}
+     [:arc {:width 60 :height 60 :start 0 :stop 3.14}]]]])
+
+(def raw-slides
+  [[:text {:value "Functional Game\nDevelopment" :halign :center}]
+   [[:text {:value "Why no functional games?" :halign :center}]
+    [:text {:value (str "1. Performance" \newline
+                     "2. Less mature libraries" \newline
+                     "3. Still not sure what it means")
+            :halign :center :size 16 :y 50}]]
+   [[:div {:x 200} smiley]
+    [:text {:value "fill(\"yellow\");
+ellipse(50, 50, 100, 100);
+fill(\"black\");
+ellipse(30, 40, 10, 10);
+ellipse(70, 40, 10, 10);
+noFill();
+arc(50, 55, 60, 60, 0, 3.14);"
+            :font "Courier New" :halign :left :size 14 :x -150 :y -50}]]
+   [:text {:value "var commands =
+  [\"fill\", {color: \"yellow\"},
+   [\"ellipse\", {width: 100, height: 100},
+    [\"fill\", {color: \"black\"},
+     [\"ellipse\", {x: -20, y: -10, width: 10, height: 10}],
+     [\"ellipse\", {x: 20, y: -10, width: 10, height: 10}]],
+    [\"fill\", {},
+     [\"arc\", {width: 60, height: 60, start: 0, stop: 3.14}]]]];
+
+render(commands);"
+           :font "Courier New" :halign :left :size 14 :x -150 :y -50}]
+   [:text {:value "(def commands
+  [:fill {:color \"yellow\"}
+   [:ellipse {:width 100 :height 100}
+    [:fill {:color \"black\"}
+     [:ellipse {:x -20 :y -10 :width 10 :height 10}]
+     [:ellipse {:x 20 :y -10 :width 10 :height 10}]]
+    [:fill {}
+     [:arc {:width 60 :height 60 :start 0 :stop 3.14}]]]])
+
+(render commands)"
+           :font "Courier New" :halign :left :size 14 :y -50}]])
+
+(def slides
+  (reduce
+    (fn [nested-slides slide]
+      [:div {:x u/view-size} slide nested-slides])
+    []
+    (reverse raw-slides)))
+
+(def main-screen
+  (reify p/Screen
+    (on-show [_]
+      (when-not @state
+        (reset! state (s/initial-state game))))
+    (on-hide [_])
+    (on-render [_]
+      (let [{:keys [x y direction current]} @state
+            koala-x (if (= direction :left) (- u/koala-offset) u/koala-offset)]
+        (p/render game [[:stroke {}
+                         [:fill {:color "lightblue"}
+                          [:rect {:width u/view-size :height u/view-size}]]]
+                        [:tiled-map {:value (:map @state) :x x}]
+                        [:div {:x (- (+ x 350)) :y 100}
+                         slides]
+                        [:div {:x koala-x :y y :width u/koala-width :height u/koala-height}
+                         current]]))
+      (reset! state
+        (-> @state
+            (s/move game)
+            (s/prevent-move game)
+            (s/animate))))
+    (on-event [_ event])))
+
+(doto game
+  (p/stop)
+  (p/start ["keydown"])
+  (p/set-screen main-screen))
+
---
+++ src/functional_game_dev/state.cljs
@@ -0,0 +1,73 @@
+(ns functional-game-dev.state
+  (:require [play-cljs.core :as p]
+            [functional-game-dev.utils :as u]))
+
+(defn initial-state [game]
+  (let [image (p/load-image game u/image-url)
+        stand-right [:image {:value image :swidth u/koala-width :sheight u/koala-height}]
+        stand-left [:image {:value image :swidth u/koala-width :sheight u/koala-height :scale-x -1 :width (- u/koala-width)}]
+        jump-right [:image {:value image :swidth u/koala-width :sheight u/koala-height :sx u/koala-width}]
+        jump-left [:image {:value image :swidth u/koala-width :sheight u/koala-height :sx u/koala-width :scale-x -1 :width (- u/koala-width)}]]
+    {:current stand-right
+     :stand-right stand-right
+     :stand-left stand-left
+     :jump-right jump-right
+     :jump-left jump-left
+     ;:walk-right TODO
+     ;:walk-left TODO
+     :x-velocity 0
+     :y-velocity 0
+     :x -100
+     :y 200
+     :can-jump? false
+     :direction :right
+     :map (p/load-tiled-map game u/map-name)}))
+
+(defn move
+  [{:keys [x y can-jump?] :as state} game]
+  (let [delta-time (min (p/get-delta-time game) 100)
+        x-velocity (u/get-x-velocity game state)
+        y-velocity (+ (u/get-y-velocity game state) u/gravity)
+        x-change (* x-velocity delta-time)
+        y-change (* y-velocity delta-time)]
+    (if (or (not= 0 x-change) (not= 0 y-change))
+      (assoc state
+             :x-velocity (u/decelerate x-velocity)
+             :y-velocity (u/decelerate y-velocity)
+             :x-change x-change
+             :y-change y-change
+             :x (+ x x-change)
+             :y (+ y y-change)
+             :can-jump? (if (neg? y-velocity) false can-jump?))
+      state)))
+
+(defn prevent-move
+  [{:keys [x y x-change y-change] :as state} game]
+  (let [old-x (- x x-change)
+        old-y (- y y-change)
+        up? (neg? y-change)]
+    (merge state
+      (when (u/touching-tile? (:map state) 1 (+ x u/koala-offset) old-y u/koala-width u/koala-height)
+        {:x-velocity 0 :x-change 0 :x old-x})
+      (when (u/touching-tile? (:map state) 1 (+ old-x u/koala-offset) y u/koala-width u/koala-height)
+        {:y-velocity 0 :y-change 0 :y old-y :can-jump? (not up?)})
+      (when (> y (- (p/get-height game) u/koala-height))
+        {:y-velocity 0 :y-change 0 :y old-y :can-jump? (not up?)}))))
+
+(defn animate
+  [{:keys [x-velocity y-velocity
+           stand-right stand-left
+           jump-right jump-left
+           walk-right walk-left] :as state}]
+  (let [direction (u/get-direction state)]
+    (-> state
+        (assoc :current
+          (cond
+            (not= y-velocity 0)
+            (if (= direction :right) jump-right jump-left)
+            ;(not= x-velocity 0)
+            ;(if (= direction :right) walk-right walk-left)
+            :else
+            (if (= direction :right) stand-right stand-left)))
+        (assoc :direction direction))))
+
